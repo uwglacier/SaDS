12a4384b8fde026dbdd27ab3d9626047b4f168bc
tghill <tghill@uwaterloo.ca>
2021-06-02 15:59:18 -0400

    Update analysis script

--- tests/SHMIP/analyze_outputs.m
+++ tests/SHMIP/analyze_outputs.m
@@ -1,13 +1,15 @@
 % Script to analyze SaDS model outputs
 % This script uses the functions in the /functions/ directory
 
+set_paths
+
 % First we read in the outputs
-outs = load('./outptus/baseline.mat');
+outs = load('./outputs/baseline.mat');
 dmesh = outs.params.dmesh;
 
 % Plot sheet water depth
 figure('Units', 'centimeters', 'Position', [4, 4, 20, 8])
-element_plot(dmesh, outs.outputs.hs(:, end))
+element_plot(dmesh, outs.outputs.hs(:, end), 'EdgeColor', 'none')
 axis image
 
 % Now make it a nicer plot
@@ -16,6 +18,8 @@ ylabel('Northing (m)')
 cmocean('dense')
 cb = colorbar;
 cb.Label.String = 'h_s (m)';
+
+print('fig_hs', '-dpng', '-r600')
 
 % Plot channel flow
 figure('Units', 'centimeters', 'Position', [3, 3, 20, 8])
@@ -26,3 +30,5 @@ xlabel('Easting (m)')
 ylabel('Northing (m)')
 cb = get(gca, 'ColorBar');
 cb.Label.String = 'h_s (m)';
+
+print('fig_qc', '-dpng', '-r600')
