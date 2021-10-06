f52912c440979486cf568ea18d9cd0729d5f7775
timghill <tghill@edu.uwaterloo.ca>
2021-10-06 07:15:07 -0400

    Try-Catch block in fprintf output

--- model/odeRK_OutputFcn.m
+++ model/odeRK_OutputFcn.m
@@ -14,6 +14,11 @@ if isempty(flag)
     progress = floor(width*(t(1) - tt(1))/(tt(end) - tt(1)));
     pstr = [repmat('.', 1, progress), repmat(' ', 1, width - progress)];
     t2 = toc(t_wall_ref);
-    fprintf('[%s] (t_model = %f, t_wall = %f, AbsConv = %f)\n', pstr, t, t2, convergence)
+    try
+        fprintf('[%s] (t_model = %f, t_wall = %f, AbsConv = %f)\n', pstr, t, t2, convergence)
+    catch
+        a=0;
+    end
+
 end
 status = 0;
